6ce488e1a109c01b6fc341b414cdc7e9a824168a
Brennangrout <bjg813@gmail.com>
2017-12-07 16:43:12 -0800

    Created hof.js for high order functions in JavaScript

---
+++ code_snippets/hof.js
@@ -0,0 +1,36 @@
+function cubeOdd(arr) {
+
+//find any non-nums, and return undefined if found
+arr.forEach(function(number){
+  if(typeof number !== 'number'){
+    return undefined;
+  }
+});
+
+// insert code here >.<
+
+//cube each number
+arr = arr.map(function(number){
+  return number * number * number;
+});
+
+console.log(arr);
+
+//filter out non-odds
+arr = arr.filter(function(number){
+  return number % 2 !== 0;
+});
+
+console.log(arr);
+
+//get sum of remaining elements
+arr = arr.reduce(function(sum,number){
+  return number + number;
+});
+
+console.log(arr);
+
+return 0;
+}
+
+cubeOdd([1,2,3,4]);
