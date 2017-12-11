fec6622d20984d3b3fd4a18ec568c2c8e5071dd5
Brennangrout <bjg813@gmail.com>
2017-12-11 15:33:26 -0800

    Created skeleton.js file for wcw 014 skeleton_03

---
+++ code_snippets/skeleton.js
@@ -0,0 +1,75 @@
+// javascript
+/*
+Flesh out the implementation and test it.
+
+Implementation of WHAT, you say? What's the problem statement???
+
+Well, you should be able to tell what this code is intended to do just from reading the starter "skeleton".
+
+Assuming you find the above statement to be true upon reading the outline, well, 
+then that illustrates the power of good outlining. You don't need a bunch of comments explaining the code. 
+The code is effectively SELF-EXPLANATORY, even at this early, not-fully-implemented stage. 
+This is the level of clarity that you should aim for in your own coding too.
+
+===
+*Some notes about the skeleton*
+
+When you create such "skeletons" for your own programs, this is a good full-fledged sample to bear in mind.
+
+Note the mixture of real "stub" code and pseudocode.
+
+The stubs are just a few function names and a few key variable names, not whole for-loops and whatnot.
+
+The pseudocode style we want for this purpose is at the level of precise English. 
+It also is not describing for-loops and whatnot. It also is only inside functions.
+Don't pseudocode functions, just write the functions as empty stubs. That saves you precious time later, with less rewriting.
+
+*/
+
+function findMaxRepeatCountInWord(word) {
+  // Break up individual words into individual letters.
+  word = word.split('');
+  console.log(word);
+  var mostCounts = 0;
+  var count = {};
+  // Count the instances of each letter
+  // Iterate all the counts and find the highest
+  // Return this word's max repeat count
+  for(var i = 0; i < word.length; i++){
+      if(count[word[i]] === undefined){
+          count[word[i]] = 0;
+      }
+      count[word[i]]++;
+  }
+  for(var prop in count){
+      if(count[prop] > mostCounts){
+          mostCounts = count[prop];
+      }
+  }
+  return mostCounts;
+}
+
+console.log(findMaxRepeatCountInWord('chocolate'));
+
+function findFirstWordWithMostRepeatedChars(text) {
+  var maxRepeatCountOverall = 0;
+  var wordWithMaxRepeatCount = '';
+
+  // Break up input text into words (space-delimited).
+  var words = text.split(' ');
+  console.log(words);
+  // For each word...
+  for(var i = 0; i < words.length; i++){
+      var repeatCountForWord = findMaxRepeatCountInWord(words[i])
+      //  If that max repeat count is higher than the overall max repeat count, then
+      if(repeatCountForWord > maxRepeatCountOverall){
+        //    update maxRepeatCountOverall
+        maxRepeatCountOverall = repeatCountForWord;
+        //    update wordWithMaxRepeatCount
+        wordWithMaxRepeatCount = words[i];
+      }
+  }
+  return wordWithMaxRepeatCount;
+}
+
+console.log(findFirstWordWithMostRepeatedChars('I like chocolate carbunkle'));
