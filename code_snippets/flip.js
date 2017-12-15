af02767f3b51ea1a16cdef624b9e701256ae1ef6
Brennangrout <bjg813@gmail.com>
2017-12-15 14:51:20 -0800

    Finished flip problem, onto the next one

---
+++ code_snippets/flip.js
@@ -0,0 +1,36 @@
+/* Flip every pair of characters in a string.
+
+
+Example:
+var input = 'check out how interesting this problem is, it\'s insanely interesting!';
+var output = flipPairs(input);
+console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
+*/
+
+//  split the string into an array
+//  loop through the array 
+//    take 2 characters at a time from the front of the array, reverse them, add them to the back of the new string
+//  return the reversed string
+
+function flipPairs (string) {
+    var splitString = string.split('');
+    var flippedString = '';
+    
+    for(var i = 0; i < string.length; i++) {
+      flippedString += splitString.splice(0, 2).reverse().join('');
+    }
+    return flippedString;
+  }
+  
+  var expected = 'hcce kuo toh wnietertsni ghtsip orlbmei ,si \'t sniasenyli tnreseitgn!';
+  var input =  "check out how interesting this problem is, it's insanely interesting!";
+  
+  function assertEqual(actual, expected, testName) {
+      if(actual === expected) {
+          console.log('Passed!');
+      } else {
+        console.log('FAILED [' + testName + '] Expected ' + expected + ', but got ' + actual);
+      }
+  }
+
+  assertEqual(flipPairs(input), expected, 'test');
