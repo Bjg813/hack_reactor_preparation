7f6f9bae37ec228ffb0bad6497bba232a1d812cf
Brennangrout <bjg813@gmail.com>
2017-12-16 15:03:23 -0800

    Created hof_refactoring.js file

--- code_snippets/outliers_1.js
+++ code_snippets/outliers_1.js
@@ -19,34 +19,43 @@ function detectOutlierValue(string) {
       //count++ for every odd and even number and record the index of the last tested number
     //if (count === 1) 
       // return that the index of that number + 1
-  
-    var array = string.split(' ');
-    var outlierIndex;
-    var evenCount = 0;
-    var oddCount = 0;
-    
-    for (i = 0; i < array.length; i++) {
-      if (Math.abs(Math.round(array[i])) % 2 === 0) {
-        evenCount++;
-        if (evenCount === 1) {
-          outlierIndex = i + 1;
-        }
-      } else {
-        oddCount++;
-        if (oddCount === 1) {
-          outlierIndex = i + 1;
+
+      var array = string.split(' ');
+      var outlierIndex;
+      var evenCount = 0;
+      var oddCount = 0;
+
+      for(var i = 0; i < array.length; i++) {
+        if(Math.abs(Math.round(array[i])) % 2 === 0) {
+          evenCount++;
+          if(evenCount === 1) {
+            outlierIndex = i + 1;
+          }
+        } else {
+          oddCount++;
+          if(oddCount === 1) {
+            outlierIndex = i + 1;
+          }
         }
       }
-    }
-    return outlierIndex;
-  }
+        return outlierIndex;
+      }
+
+      function findEvenNumbers(num) {
+        let evenNumbers = num.filter(number => number % 2 === 0);
+        return evenNumbers;
+        // const result = words.filter(word => word.length > 6);
+      }
+
+      ///////////////// Testing ///////////////////////////////////////////////////
+
+      assertEqual(findEvenNumbers(num), )
+
   
-  function checkOutlier(expected, actual) {
-    if (expected === actual) {
-      console.log('PASSED');
-    } else {
-      console.log('FAILED "' + expected + '" does not equal "' + actual + '"');
-    }
+function assertEqual(actual, expected, testName) {
+  if(actual === expected) {
+    console.log('Passed!');
+  } else {
+    console.log('FAILED [' + testName + '] Expected "' + expected + '", but got "' + actual + '"');
   }
-  
-  checkOutlier(detectOutlierValue('-1.2 1.9 11.555 -5'), 2)
+}
