40882ed51038294694a8e18f0321ded0fc126989
Brennangrout <bjg813@gmail.com>
2017-12-16 16:06:33 -0800

    Finished getElementsLessThan100 function using filter HOF

--- code_snippets/hof_refactoring.js
+++ code_snippets/hof_refactoring.js
@@ -14,7 +14,41 @@ var obj = {
 
 function getElementsThatEqual10AtProperty(obj, key) {
     if(!obj[key] || !Array.isArray(obj[key])) return [];
-    console.log(obj[key].filter((a) => a === 10));
+    return obj[key].filter((a) => a === 10);
 }
 
-getElementsThatEqual10AtProperty(obj, 'key');
+// Write a function called “getElementsLessThan100AtProperty”.
+
+// Given an object and a key, “getElementsLessThan100AtProperty” returns an array containing all the elements of the array located at the given key that are less than 100.
+
+// Notes:
+// * If the array is empty, it should return an empty array.
+// * If the array contains no elements less than 100, it should return an empty array.
+// * If the property at the given key is not an array, it should return an empty array.
+// * If there is no property at the key, it should return an empty array.
+
+function getElementsLessThan100AtProperty(obj, key) {
+    if(!obj[key] || !Array.isArray(obj[key])) return [];
+    return obj[key].filter((a) => a < 100);
+}
+
+/////////////// Testing ///////////////////////////////////////////////////////////////////////////////
+
+assertArraysEqual(getElementsThatEqual10AtProperty(obj, 'key'), [10, 10], 'test');
+assertArraysEqual(getElementsLessThan100AtProperty(obj, 'key'), [10, 50, 10], 'Test for numbers less than 100');
+
+
+function assertArraysEqual(actual, expected, testName) {
+    var count = 0;
+    for ( i = 0; i <= expected.length; i++ ) {
+      if ( actual[i] !== expected[i] ) {
+        count++;
+      }
+    }
+    if ( count === 0 ) {
+      console.log('passed');  
+    } else if ( count > 0 || expected.length !== actual.length ) {
+      console.log('FAILED ' + '[' + testName + ']' + ' Expected ' + '"' + expected + '",' + ' but got ' + '"' + actual + '"');
+    }
+  }
+
