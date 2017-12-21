369d8711290db2da30b60ff0f94c79a7ba2c181d
Brennangrout <bjg813@gmail.com>
2017-12-20 23:38:13 -0800

    Updated outliers.js

--- code_snippets/outliers.js
+++ code_snippets/outliers.js
@@ -2,7 +2,7 @@
 
 // The return value should be 1-indexed, not 0-indexed.
 
-// Examples :
+// Examples in time:
 // detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest of the numbers are even
 // detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest of the numbers are odd
 
